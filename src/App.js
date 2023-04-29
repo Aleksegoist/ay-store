616a689fbf7835d7523124d93a0a9b088b55fb42
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-04-29 22:04:29 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+//pages
+import { Cart, Category, Home } from './pages/index';
+//components
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className='App'>
-      <div className='container'>
-        <h1>Hello World!</h1>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/category/:id' element={<Category />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
