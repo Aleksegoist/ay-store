3f1347ea8cb02560ad1ddad3b9d267856d55e5d1
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-04-29 21:46:25 +0300

    added structur

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Cart from './CartPage/CartPage';
+import Category from './CategoryPage/CategoryPage';
+import Home from './HomePage/HomePage';
+
+export { Home, Category, Cart };
---
+++ src/utils/images.js
@@ -0,0 +1,5 @@
+import error from '../assets/images/error.png';
+import slider_img_1 from '../assets/images/slider-img-1.png';
+import spinner from '../assets/images/spinner.svg';
+
+export { slider_img_1, spinner, error };
